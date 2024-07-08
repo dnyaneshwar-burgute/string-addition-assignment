e88dfbdb72f82823e10df8315ce7733fe090a8ce
dnyanesh-pathfactory <dnyaneshwar.burgute@pathfactory.com>
2024-07-08 12:18:53 +0530

    worked on the test case 2

--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -16,4 +16,8 @@ RSpec.describe "StringCalculator" do
   it 'should return 6 if sent the numbers as "1,5"' do
     expect(@calculator.add('1,5')).to eq(6)
   end
+
+  it 'should pass any amount of numbers' do
+    expect(@calculator.add('1,5,4,2')).to eq(12)
+  end
 end
